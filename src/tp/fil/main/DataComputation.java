8c79768466e83605e347ee044e0a4e2234e6801e
mmainchain <mael@mainchain.fr>
2019-02-14 10:39:08 +0100

    Initial commit

---
+++ src/tp/fil/main/DataComputation.java
@@ -0,0 +1,62 @@
+package tp.fil.main;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.gmt.modisco.java.emf.JavaPackage;
+
+public class DataComputation {
+	
+	public static void main(String[] args) {
+		try {
+			Resource javaModel;
+			Resource dataModel;
+			Resource dataMetamodel;
+			
+			ResourceSet resSet = new ResourceSetImpl();
+			resSet.getResourceFactoryRegistry().
+				getExtensionToFactoryMap().
+				put("ecore", new EcoreResourceFactoryImpl());
+			resSet.getResourceFactoryRegistry().
+				getExtensionToFactoryMap().
+				put("xmi", new XMIResourceFactoryImpl());
+			
+			JavaPackage.eINSTANCE.eClass();
+			
+			dataMetamodel = resSet.createResource(URI.createFileURI("src/tp/fil/resources/Data.ecore"));
+			dataMetamodel.load(null);
+			EPackage.Registry.INSTANCE.put("http://data", 
+					dataMetamodel.getContents().get(0));
+			
+			javaModel = resSet.createResource(URI.createFileURI("../PetStore/PetStore_java.xmi"));
+			javaModel.load(null);
+			
+			dataModel = resSet.createResource(URI.createFileURI("../PetStore/PetStore_data.xmi"));
+			
+			/*
+			 * Beginning of the part to be completed...
+			 */
+			
+			//TO BE COMPLETED
+			
+			/*
+			 * End of the part to be completed...
+			 */
+			
+			dataModel.save(null);
+			
+			javaModel.unload();
+			dataModel.unload();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
