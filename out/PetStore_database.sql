9fe37fdf7ee2db0e350f09b16ccaa7ca41d24863
44chloeguilbaud@gmail.com <44chloeguilbaud@gmail.com>
2019-02-20 12:58:34 +0100

    feat: SQL Generation from xmi

--- out/PetStore_database.sql
+++ out/PetStore_database.sql
@@ -1,84 +1,140 @@
-	
 		CREATE TABLE SellerContactInfo (
-				contactInfoID VARCHAR,
-				lastName VARCHAR,
-				firstName VARCHAR,
-				email VARCHAR,
+					contactInfoID VARCHAR,
+					
+					lastName VARCHAR,
+					
+					firstName VARCHAR,
+					
+					email VARCHAR,
+					
 		)
 		CREATE TABLE Tag (
-				tagID VARCHAR,
-				items VARCHAR,
-				tag VARCHAR,
-				refCount VARCHAR,
+					tagID INT,
+					
+					items  VARCHAR,
+					
+					tag VARCHAR,
+					
+					refCount INT,
+					
 		)
 		CREATE TABLE Address (
-				addressID VARCHAR,
-				street1 VARCHAR,
-				street2 VARCHAR,
-				city VARCHAR,
-				state VARCHAR,
-				zip VARCHAR,
-				latitude VARCHAR,
-				longitude VARCHAR,
-				COMMA VARCHAR,
+					addressID VARCHAR,
+					
+					street1 VARCHAR,
+					
+					street2 VARCHAR,
+					
+					city VARCHAR,
+					
+					state VARCHAR,
+					
+					zip VARCHAR,
+					
+					latitude  VARCHAR,
+					
+					longitude  VARCHAR,
+					
+					COMMA VARCHAR,
+					
 		)
 		CREATE TABLE FileUploadResponse (
-				itemId VARCHAR,
-				productId VARCHAR,
-				message VARCHAR,
-				status VARCHAR,
-				duration VARCHAR,
-				durationString VARCHAR,
-				startDate VARCHAR,
-				endDate VARCHAR,
-				uploadSize VARCHAR,
-				thumbnail VARCHAR,
+					itemId VARCHAR,
+					
+					productId VARCHAR,
+					
+					message VARCHAR,
+					
+					status VARCHAR,
+					
+					duration VARCHAR,
+					
+					durationString VARCHAR,
+					
+					startDate VARCHAR,
+					
+					endDate VARCHAR,
+					
+					uploadSize VARCHAR,
+					
+					thumbnail VARCHAR,
+					
 		)
 		CREATE TABLE Category (
-				categoryID VARCHAR,
-				name VARCHAR,
-				description VARCHAR,
-				imageURL VARCHAR,
+					categoryID VARCHAR,
+					
+					name VARCHAR,
+					
+					description VARCHAR,
+					
+					imageURL VARCHAR,
+					
 		)
 		CREATE TABLE RatingBean (
-				itemId VARCHAR,
-				grade VARCHAR,
-				cf VARCHAR,
+					itemId VARCHAR,
+					
+					grade INT,
+					
+					cf  VARCHAR,
+					
 		)
 		CREATE TABLE PayPalBean (
-				postData VARCHAR,
+					postData  VARCHAR,
+					
 		)
 		CREATE TABLE ZipLocation (
-				zipCode VARCHAR,
-				city VARCHAR,
-				state VARCHAR,
+					zipCode INT,
+					
+					city VARCHAR,
+					
+					state VARCHAR,
+					
 		)
 		CREATE TABLE Item (
-				itemID VARCHAR,
-				productID VARCHAR,
-				name VARCHAR,
-				description VARCHAR,
-				imageURL VARCHAR,
-				imageThumbURL VARCHAR,
-				price VARCHAR,
-				address VARCHAR,
-				contactInfo VARCHAR,
-				totalScore VARCHAR,
-				numberOfVotes VARCHAR,
-				disabled VARCHAR,
-				tags VARCHAR,
+					itemID VARCHAR,
+					
+					productID VARCHAR,
+					
+					name VARCHAR,
+					
+					description VARCHAR,
+					
+					imageURL VARCHAR,
+					
+					imageThumbURL VARCHAR,
+					
+					price  VARCHAR,
+					
+					address  VARCHAR,
+					
+					contactInfo  VARCHAR,
+					
+					totalScore INT,
+					
+					numberOfVotes INT,
+					
+					disabled INT,
+					
+					tags  VARCHAR,
+					
 		)
 		CREATE TABLE Product (
-				productID VARCHAR,
-				categoryID VARCHAR,
-				name VARCHAR,
-				description VARCHAR,
-				imageURL VARCHAR,
+					productID VARCHAR,
+					
+					categoryID VARCHAR,
+					
+					name VARCHAR,
+					
+					description VARCHAR,
+					
+					imageURL VARCHAR,
+					
 		)
 		CREATE TABLE CatalogFacade (
-				emf VARCHAR,
-				utx VARCHAR,
-				bDebug VARCHAR,
+					emf  VARCHAR,
+					
+					utx  VARCHAR,
+					
+					bDebug  VARCHAR,
+					
 		)
-	
-
